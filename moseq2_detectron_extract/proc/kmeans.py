dad82bfb45326b870bc3371d9f52278bfd5b3cd2
Josh Thackray <thackray@rutgers.edu>
2021-11-30 16:12:41 -0500

    allow running kmeans selection on subset of frames

--- moseq2_detectron_extract/proc/kmeans.py
+++ moseq2_detectron_extract/proc/kmeans.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 import numpy as np
 import tqdm
 from moseq2_detectron_extract.io.session import Session
@@ -8,13 +8,14 @@ from skimage.transform import resize
 from sklearn.cluster import MiniBatchKMeans
 
 
-def select_frames_kmeans(session: Session, num_frames_to_pick: int, num_clusters: int=None, chunk_size: int=1000,
+def select_frames_kmeans(session: Session, num_frames_to_pick: int, indices: Optional[List[int]]=None, num_clusters: int=None, chunk_size: int=1000,
     scale: float=4, min_height: int=0, max_height: int=100, kmeans_batchsize: int=100, kmeans_max_iter: int=50) -> List[int]:
     ''' Select frames from a session using k-means clustering to pick dissimilar frames
 
     Parameters:
     session (Session): Session from which to pick frames
     num_frames_to_pick (int): Total number of frames to pick from this session
+    indices (List[int]|None): Subset of indices to pick cluster from
     num_clusters (int): Number of (k-means) clusters to use. If none, then `num_frames_to_pick` clusters are used
     chunk_size (int): Number of frames to read from session in an iteration
     scale (float): Down-sample images by this scale factor
@@ -24,17 +25,29 @@ def select_frames_kmeans(session: Session, num_frames_to_pick: int, num_clusters
     kmeans_max_iter (int): Max iterations for fitting k-means model
 
     Returns:
-    list of frame indicies which were selected
+    list of frame indices which were selected
     '''
 
     _, bground_im, roi, _ = session.find_roi()
-    downsampled = np.zeros((session.nframes, int(roi.shape[0] / scale), int(roi.shape[1] / scale)))
-    for frame_idxs, raw_frames in tqdm.tqdm(session.iterate(chunk_size=chunk_size), desc='Processing batches', leave=False):
+    
+
+    if indices is None:
+        frames_iterator = session.iterate(chunk_size=chunk_size)
+        nframes = session.nframes
+    else:
+        frames_iterator = session.index(indices, chunk_size=chunk_size)
+        nframes = len(indices)
+
+    downsampled = np.zeros((nframes, int(roi.shape[0] / scale), int(roi.shape[1] / scale)))
+    all_frame_idxs = np.empty((nframes,), dtype=int)
+    for b, (frame_idxs, raw_frames) in enumerate(tqdm.tqdm(frames_iterator, desc='Processing batches', leave=False)):
         raw_frames = prep_raw_frames(raw_frames, bground_im=bground_im, roi=roi, vmin=min_height, vmax=max_height)
         raw_frames = scale_raw_frames(raw_frames, vmin=min_height, vmax=max_height)
 
         for i, idx in enumerate(tqdm.tqdm(frame_idxs, desc='Resizing Frames', leave=False, disable=False)):
-            downsampled[idx, :, :] = resize(raw_frames[i], downsampled.shape[1:], anti_aliasing=True, preserve_range=True, mode='constant')
+            sparse_idx = ((b * chunk_size) + i)
+            downsampled[sparse_idx, :, :] = resize(raw_frames[i], downsampled.shape[1:], anti_aliasing=True, preserve_range=True, mode='constant')
+            all_frame_idxs[sparse_idx] = idx
 
     with tqdm.tqdm(total=1, leave=False, desc="Kmeans clustering ... (this might take a while)") as pbar:
         data = downsampled - downsampled.mean(axis=0)
@@ -59,6 +72,7 @@ def select_frames_kmeans(session: Session, num_frames_to_pick: int, num_clusters
     selected_frames = []
     for cluster_id in range(num_clusters):  # pick one frame per cluster
         cluster_ids = np.where(cluster_id == kmeans.labels_)[0]
+        cluster_ids = all_frame_idxs[cluster_ids]
 
         num_images_in_cluster = len(cluster_ids)
         if num_images_in_cluster > 0:
