ce42e61447b307189c3f3e65a360d62412fa72c8
Josh Thackray <thackray@rutgers.edu>
2022-03-15 14:36:12 -0400

    spill keypoints to disk on each batch, rather than only on completion

--- moseq2_detectron_extract/pipeline/keypoint_writer_step.py
+++ moseq2_detectron_extract/pipeline/keypoint_writer_step.py
@@ -11,17 +11,16 @@ class KeypointWriterStep(PipelineStep):
     '''
 
     def initialize(self):
-        self.kp_out_data = []
+        self.keypoint_data_dest = os.path.join(self.config['output_dir'], f"keypoints_{self.config['bg_roi_index']:02d}.tsv")
+        self.kp_out_data = None
 
-    def finalize(self):
-        keypoint_data_dest = os.path.join(self.config['output_dir'], f"keypoints_{self.config['bg_roi_index']:02d}.tsv")
-        pd.DataFrame(self.kp_out_data).to_csv(keypoint_data_dest, sep='\t', index=False)
 
     def process(self, data) -> None:
         nframes = data['chunk'].shape[0]
-        self.update_progress(nframes)
+
+        kp_data = []
         for i in range(nframes):
-            self.kp_out_data.append({
+            kp_data.append({
                 'Frame_Idx': data['frame_idxs'][i],
                 'Flip': data['features']['flips'][i],
                 'Centroid_X': data['features']['features']['centroid'][i][0],
@@ -30,3 +29,10 @@ class KeypointWriterStep(PipelineStep):
                 **slice_dict(data['keypoints'], i)
             })
             self.update_progress()
+
+        if self.kp_out_data is None:
+            self.kp_out_data = pd.DataFrame(kp_data)
+        else:
+            self.kp_out_data = pd.concat((self.kp_out_data, pd.DataFrame(kp_data)), ignore_index=True)
+
+        self.kp_out_data.to_csv(self.kp_out_data, sep='\t', index=False)
