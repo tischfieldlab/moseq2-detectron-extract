7343c5d858bfa9b5b1f319173a4b03d9c6995a63
quantumdot <quantumdot@users.noreply.github.com>
2023-02-01 11:25:22 -0500

    make preview video step utilize threads to speed up video generation

--- moseq2_detectron_extract/pipeline/preview_video_writer_step.py
+++ moseq2_detectron_extract/pipeline/preview_video_writer_step.py
@@ -1,16 +1,22 @@
 
+import math
 import os
+import time
+from concurrent.futures import ThreadPoolExecutor
 from functools import partial
+from typing import Optional
 
 import numpy as np
 from detectron2.data import MetadataCatalog
+
+from moseq2_detectron_extract.io.util import gen_batch_sequence
 from moseq2_detectron_extract.io.video import PreviewVideoWriter
 from moseq2_detectron_extract.pipeline.pipeline_step import ProcessPipelineStep
 from moseq2_detectron_extract.proc.keypoints import \
     load_keypoint_data_from_dict
 from moseq2_detectron_extract.proc.proc import stack_videos
-from moseq2_detectron_extract.viz import ArenaView, CleanedFramesView, RotatedKeypointsView
-from typing import Optional
+from moseq2_detectron_extract.viz import (ArenaView, CleanedFramesView,
+                                          RotatedKeypointsView)
 
 # pylint: disable=attribute-defined-outside-init
 
@@ -22,6 +28,7 @@ class PreviewVideoWriterStep(ProcessPipelineStep):
 
         # We need to grab the metadata for the dataset here, or the data will not be available in the subprocess!!
         self.dset_meta = MetadataCatalog.get(self.config['dataset_name'])
+        self.workers = 4
 
     def initialize(self):
         preview_video_dest = os.path.join(self.config['output_dir'], f"results_{self.config['bg_roi_index']:02d}.mp4")
@@ -47,7 +54,10 @@ class PreviewVideoWriterStep(ProcessPipelineStep):
                                      units='px',
                                      root='')
 
+        self.executor = ThreadPoolExecutor(self.workers)
+
     def finalize(self):
+        self.executor.shutdown()
         self.video_pipe.close()
 
     def process(self, data):
@@ -68,6 +78,39 @@ class PreviewVideoWriterStep(ProcessPipelineStep):
         ref_masks = np.array(ref_masks)
         ref_keypoints = np.array(ref_keypoints)
         ref_boxes = np.array(ref_boxes)
+        frame_idxs = np.array(data['frame_idxs'])
+
+        # field_video = self.arena_view.generate_frames(raw_frames=raw_frames, boxes=ref_boxes, keypoints=ref_keypoints, masks=ref_masks)
+        # rc_kpts_video = self.rot_kpt_view.generate_frames(masks=masks, keypoints=rot_keypoints)
+        # cln_depth_video = self.clean_frames_view.generate_frames(clean_frames=clean_frames, masks=masks)
+
+        # proc_stack = stack_videos([cln_depth_video, rc_kpts_video], orientation='vertical')
+        # out_video_combined = stack_videos([proc_stack, field_video], orientation='horizontal')
+
+        futures = []
+        nframes = raw_frames.shape[0]
+        batch_size = math.ceil(nframes / self.workers)
+        batches = [list(batch_idxs) for batch_idxs in gen_batch_sequence(nframes, batch_size)]
+        for batch_idxs in batches:
+            futures.append(self.executor.submit(self._process_chunk,
+                                                frame_idxs[batch_idxs],
+                                                raw_frames[batch_idxs],
+                                                ref_boxes[batch_idxs],
+                                                ref_keypoints[batch_idxs],
+                                                ref_masks[batch_idxs],
+                                                masks[batch_idxs],
+                                                rot_keypoints[batch_idxs],
+                                                clean_frames[batch_idxs]))
+
+        for future in futures:
+            while not future.done():
+                time.sleep(0.5)
+
+            frame_idxs, out_video_combined = future.result()
+            self.video_pipe.write_frames(frame_idxs, out_video_combined)
+            self.update_progress(frame_idxs.shape[0])
+
+    def _process_chunk(self, frame_idxs, raw_frames, ref_boxes, ref_keypoints, ref_masks, masks, rot_keypoints, clean_frames):
 
         field_video = self.arena_view.generate_frames(raw_frames=raw_frames, boxes=ref_boxes, keypoints=ref_keypoints, masks=ref_masks)
         rc_kpts_video = self.rot_kpt_view.generate_frames(masks=masks, keypoints=rot_keypoints)
@@ -75,5 +118,5 @@ class PreviewVideoWriterStep(ProcessPipelineStep):
 
         proc_stack = stack_videos([cln_depth_video, rc_kpts_video], orientation='vertical')
         out_video_combined = stack_videos([proc_stack, field_video], orientation='horizontal')
-        self.video_pipe.write_frames(data['frame_idxs'], out_video_combined)
-        self.update_progress(raw_frames.shape[0])
+
+        return frame_idxs, out_video_combined
